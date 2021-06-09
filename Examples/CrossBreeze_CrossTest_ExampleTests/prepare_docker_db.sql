8152ee80f9eed371e747aac1a15814d834f3ccba
Willem <willem@x-breeze.com>
2021-06-09 21:54:51 +0200

    Fix db create script

--- Examples/CrossBreeze_CrossTest_ExampleTests/prepare_docker_db.sql
+++ Examples/CrossBreeze_CrossTest_ExampleTests/prepare_docker_db.sql
@@ -1,20 +1,36 @@
 CREATE DATABASE  ExampleDatabase
 GO
 
-USE ExampleDatabase
+USE [ExampleDatabase]
+GO
 
-Create table ExampleStagingTable (
-	ExampleColumn varchar(100),
-	StageDateTime datetime2(3)
+Create table [ExampleStagingTable] (
+	[ExampleColumn] varchar(100),
+	[StageDateTime] datetime2(3)
 )
 GO
 
-Create function dbo.GetExampleStagingTable() Returns TABLE
+Create function [dbo].[GetExampleStagingTable]() Returns TABLE
 AS 
 	Return
-		Select ExampleColumn, StageDateTime
-		FROM ExampleStagingTable
+		Select [ExampleColumn], [StageDateTime]
+		FROM [ExampleStagingTable]
+GO
 
+Create Schema [Schema With Space]
+GO
+
+Create table [Schema With Space].[ExampleStagingTableWith Space] (
+	[ExampleColumnWith Space] varchar(100),
+	[StageDateTime] datetime2(3)
+)
+GO
+
+Create function [dbo].[GetExampleStagingTableWith Space]() Returns TABLE
+AS 
+	Return
+		Select [ExampleColumnWith Space], [StageDateTime]
+		FROM [ExampleStagingTableWith Space]
 GO
 
 CREATE DATABASE [TestDB]
@@ -202,5 +218,60 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
+
+declare @Owner_LoginName as nvarchar(100)
+set @Owner_LoginName = SYSTEM_USER
+
+
+/****** Object:  Job [testjob]    Script Date: 2020-01-10 4:06:36 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 2020-01-10 4:06:36 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'TestJobWith Space', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name= @Owner_LoginName, @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [insert customer data]    Script Date: 2020-01-10 4:06:36 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'insert customer data', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'SELECT * FROM dbo.ExampleStagingTable', 
+		@database_name=N'ExampleDatabase', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
 
 
