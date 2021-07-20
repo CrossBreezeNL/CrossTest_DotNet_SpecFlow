0aa20e9fbfcc9ab5ca33f160bf71f8c8a9b0a1c1
Harmen Wessels <harmen@X-Breeze.com>
2021-07-20 22:51:53 +0200

    Updated release to 1.0.2.2.

--- Lib/PushNuPkg.cmd
+++ Lib/PushNuPkg.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Pushing NuPkg file to NuGet...
-call nuget push CrossBreeze.CrossTest.1.0.2.1.nupkg oy2phxtj2ms7fv3yb2yq6bdpvlwxtn4unepkzwfrlfqpq4 -Source https://api.nuget.org/v3/index.json
+call nuget push CrossBreeze.CrossTest.1.0.2.2.nupkg oy2phxtj2ms7fv3yb2yq6bdpvlwxtn4unepkzwfrlfqpq4 -Source https://api.nuget.org/v3/index.json
 echo Done.
 
 pause
