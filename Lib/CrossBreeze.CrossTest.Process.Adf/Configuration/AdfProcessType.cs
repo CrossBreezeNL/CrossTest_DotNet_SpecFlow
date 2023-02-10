00421b92e6d57b9340f4fd38043221b642860b15
Moo Othman <Moo@X-Breeze.com>
2023-02-10 11:25:21 +0100

    Adding ADF project again!

---
+++ Lib/CrossBreeze.CrossTest.Process.Adf/Configuration/AdfProcessType.cs
@@ -0,0 +1,14 @@
+﻿using CrossBreeze.CrossDoc.CustomAttributes;
+
+namespace CrossBreeze.CrossTest.Process.Adf.Configuration
+{
+    public enum AdfProcessType
+    {
+        [XDoc(
+            Title = "Azure Data Factory Pipeline",
+            Description = "This type is used to execute a pipeline stored on azure data factory."
+        )]
+        AdfPipeline
+
+    }
+}
