947e21eb1f3322b3de24c28c6f2ee77f0b0ed757
Moo Othman <Moo@X-Breeze.com>
2023-02-24 12:23:49 +0100

    Update Documentation For ADF

--- CrossTestDocumenter/CrossTestDocumenter.cs
+++ CrossTestDocumenter/CrossTestDocumenter.cs
@@ -11,36 +11,36 @@ namespace CrossTestDocumenter
         {
 
             // Load CrossBreeze CrossTest Core assembly
-            Assembly configAssembly = Assembly.LoadFrom("C:\\git\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_SpecFlow_Core\\bin\\Debug\\CrossBreeze.CrossTest.SpecFlow.Core.dll");
+            Assembly configAssembly = Assembly.LoadFrom("C:\\Git\\Repos\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_SpecFlow_Core\\bin\\Debug\\CrossBreeze.CrossTest.SpecFlow.Core.dll");
             // Get the Core Config type.
             Type configType = configAssembly.GetType("CrossBreeze.CrossTest.SpecFlow.Configuration.CrossTestConfig");
             // Create the specflow config documentation.
             new CrossTestConfigDocumenter().CreateConfigDocumentation
             (
                 configType,
-                "C:\\git\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\CrossTestConfig.xml"
+                "C:\\Git\\Repos\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\CrossTestConfig.xml"
             );
 
             // Load CrossBreeze CrossTest Ssis assembly
-            Assembly ssisProcessConfigAssembly = Assembly.LoadFrom("C:\\git\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_Process_Ssis\\bin\\Debug\\CrossBreeze.CrossTest.Process.Ssis.dll");
+            Assembly ssisProcessConfigAssembly = Assembly.LoadFrom("C:\\Git\\Repos\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_Process_Ssis\\bin\\Debug\\CrossBreeze.CrossTest.Process.Ssis.dll");
             // Get the Ssis Config type.
             Type ssisConfigType = ssisProcessConfigAssembly.GetType("CrossBreeze.CrossTest.Process.Ssis.Configuration.SsisProcessesConfig");
             // Create the specflow config documentation.
             new CrossTestConfigDocumenter().CreateConfigDocumentation
             (
                 ssisConfigType,
-                "C:\\git\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\SsisProcessesConfig.xml"
+                "C:\\Git\\Repos\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\SsisProcessesConfig.xml"
             );
 
             // Load CrossBreeze CrossTest Adf assembly
-            Assembly AdfProcessConfigAssembly = Assembly.LoadFrom("C:\\git\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_Process_Adf\\bin\\Debug\\CrossBreeze.CrossTest.Process.Adf.dll");
+            Assembly AdfProcessConfigAssembly = Assembly.LoadFrom("C:\\Git\\Repos\\CrossBreeze\\CrossTest\\Crosstest_DotNet_SpecFlow\\Lib\\CrossBreeze_CrossTest_Process_Adf\\bin\\Debug\\CrossBreeze.CrossTest.Process.Adf.dll");
             // Get the Adf Config type.
             Type AdfConfigType = AdfProcessConfigAssembly.GetType("CrossBreeze.CrossTest.Process.Adf.Configuration.AdfProcessesConfig");
             // Create the specflow config documentation.
             new CrossTestConfigDocumenter().CreateConfigDocumentation
             (
                 AdfConfigType,
-                "C:\\git\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\AdfProcessesConfig.xml"
+                "C:\\Git\\Repos\\CrossBreeze\\CrossTest\\CrossTest\\Generation\\Model\\AdfProcessesConfig.xml"
             );
 
         }
--- Lib/CrossBreeze_CrossTest_Process_Adf/Configuration/AdfProjectConfig.cs
+++ Lib/CrossBreeze_CrossTest_Process_Adf/Configuration/AdfProjectConfig.cs
@@ -15,36 +15,37 @@ namespace CrossBreeze.CrossTest.Process.Adf.Configuration
 
         // ProcessType
         [ConfigurationProperty("processType", IsRequired = true, DefaultValue = AdfProcessType.AdfPipeline)]
+        [XDoc(Description = "Only \"AdfPipeline\" is allowed at this moment.")]
         public AdfProcessType ProcessType => (AdfProcessType)this["processType"];
 
         // tenantID 
         [ConfigurationProperty("tenantID", IsRequired = true)]
-        [XDoc("The tenantID of the Azure environment should be specified")]
+        [XDoc("The ID of the azure environment you want to connect with.")]
         public string tenantID => this["tenantID"] as string;
 
         // subscriptionId 
         [ConfigurationProperty("subscriptionId", IsRequired = true)]
-        [XDoc("The subscriptionId of the Azure environment should be specified")]
+        [XDoc("The ID of the azure subscription on which the datafactory is located")]
         public string subscriptionId => this["subscriptionId"] as string;
 
         // applicationId (This app should be registered on Azure active directory and should have the role to write on ADF)
         [ConfigurationProperty("applicationId", IsRequired = true)]
-        [XDoc("The applicationId of the application that has been registered on Azure AD should be specified")]
+        [XDoc("The ID of the app that got registered to Azure AD and has access to the datafactory on which the pipeline is located")]
         public string applicationId => this["applicationId"] as string;
 
         // clientSecret (this secret should be created on the application and can have a maximum lifetime of 2 years)
         [ConfigurationProperty("clientSecret", IsRequired = true)]
-        [XDoc("The clientSecret of the application should be specified")]
+        [XDoc("The secret of the application")]
         public string clientSecret => this["clientSecret"] as string;
 
         // resourceGroupName 
         [ConfigurationProperty("resourceGroupName", IsRequired = true)]
-        [XDoc("The resourceGroupName where the datafactory has been made should be specified")]
+        [XDoc("The resourceGroup on which the datafactory is located")]
         public string resourceGroupName => this["resourceGroupName"] as string;
 
         // dataFactoryName
         [ConfigurationProperty("dataFactoryName", IsRequired = true)]
-        [XDoc("The dataFactoryName where the pipeline has been created should be specified")]
+        [XDoc("The datafactory on which the pipeline is located")]
         public string dataFactoryName => this["dataFactoryName"] as string;
 
         //// pipelineName 
